95ca0b1ff5ff3ce0e46084e691fb34a187269a79
Roberto Manfreda <robertomanfreda1994@gmail.com>
2021-05-04 02:06:19 +0200

    Added iteration counter and more logs

--- js/main.js
+++ js/main.js
@@ -16,8 +16,12 @@ context.canvas.height = worldHeight;
 
 const cells = [];
 
+let iterationCounter = 0;
+
 function init() {
-    console.log('-----');
+    console.log('Started initialization process..')
+
+    console.log('----- CELLS -----');
     console.log('Requested generation of a world with a total of  ' + targetCells + ' cells.');
 
     let currX = 0;
@@ -38,16 +42,24 @@ function init() {
     }
 
     console.log('Successfully generated the world with a total of ' + actualCells + ' cells.');
-    console.log('-----');
-    console.log('Cell sizes');
+
+    console.log('\n----- WORLD SIZE -----');
+    console.log('Width : ' + worldWidth + 'px');
+    console.log('Height: ' +  worldHeight + 'px');
+
+    console.log('\n----- CELLS SIZE -----');
     console.log('Width : ' + cellWidth + 'px');
     console.log('Height: ' +  cellHeight + 'px');
     console.log('-----');
+    console.log('World generation completed.')
 }
 
 function updateWorld() {
     cells.forEach((cell) => cell.draw());
     cells.forEach((cell) => cell.update());
+
+    iterationCounter++;
+    console.log('Iteration: ' + iterationCounter);
 }
 
 init();
